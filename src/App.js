31c61245a5f26471c8ddb6ec29ee31ec72f69e8a
joaquinrapetti <j.rapetti92@gmail.com>
2022-05-06 17:02:19 -0300

    finish

--- src/App.js
+++ src/App.js
@@ -1,8 +1,44 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Routes, Route } from "react-router-dom";
+import About from "./components/About";
+import Home from "./components/Home";
+import MovieView from "./components/MovieView";
+import NavBar from "./components/NavBar";
+import SearchView from "./components/SearchView";
+
 function App() {
+  const [searchResults, setSearchResults] = useState([]);
+  const [searchText, setSearchText] = useState("");
+
+  useEffect(() => {
+    if (searchText) {
+      fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${process.env.REACT_APP_MOVIES_API_KEY}&language=en-US&query=${searchText}&page=1&include_adult=false`
+      )
+        .then((response) => response.json())
+        .then((data) => {
+          setSearchResults(data.results);
+        });
+    }
+  }, [searchText]);
+
   return (
     <div>
-      <h1>Hello</h1>
+      <NavBar searchText={searchText} setSearchText={setSearchText} />
+      <Routes>
+        <Route path="/" exact element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route
+          path="/search"
+          element={
+            <SearchView keyword={searchText} searchResults={searchResults} />
+          }
+        />
+        <Route path="/movies/:id" element={<MovieView />} />
+        {/* <Route path="/search">
+          <SearchView keyword={searchText} searchResults={searchResults} />
+        </Route> */}
+      </Routes>
     </div>
   );
 }
